7c5e90d06f5a250a49b292fd3921fa516de83cf3
Nientai Ho <hntddt123@gmail.com>
2025-03-06 15:57:49 +0800

    Updated test seed files for foreign key setup

---
+++ TripBorderBackend/seeds/01_test_useraccounts_seed.js
@@ -0,0 +1,15 @@
+exports.seed = function seed(knex) {
+  // Deletes ALL existing entries
+  return knex('user_accounts').del()
+    .then(() => knex('user_accounts').insert([
+      {
+        uuid: knex.fn.uuid(),
+        email: 'test@tripborder.com',
+        provider: 'tripborder',
+        provider_user_id: '1',
+        name: 'Test',
+        created_at: knex.fn.now(),
+        updated_at: knex.fn.now()
+      },
+    ]));
+};
