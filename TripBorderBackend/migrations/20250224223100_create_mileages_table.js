d540a584e29bae7e9459c53ad9985642a03c25e1
Nientai Ho <hntddt123@gmail.com>
2025-03-06 15:57:19 +0800

    Updated mileages table with owner_email as foreign key from users

--- TripBorderBackend/migrations/20250224223100_create_mileages_table.js
+++ TripBorderBackend/migrations/20250224223100_create_mileages_table.js
@@ -4,16 +4,23 @@
  */
 exports.up = function createMileagesTable(knex) {
   return knex.schema.createTable('mileages', (table) => {
-    table.increments('id').primary();
+    table.uuid('uuid').primary().defaultTo(knex.fn.uuid());
     table.string('frequent_flyer_number').unique().notNullable();
     table.string('airline').notNullable();
     table.decimal('mileage_price', 10, 2).notNullable();
     table.decimal('mileage_amount', 12, 2).notNullable();
     table.enum('mileage_unit', ['miles', 'km']).notNullable();
-    table.string('mileage_picture').notNullable();
-    table.timestamp('mileage_expired_at').notNullable().defaultTo('9999-12-31');
+    table.specificType('mileage_picture', 'BYTEA').notNullable();
+    table.timestamp('mileage_expired_at').notNullable().defaultTo('9999-12-2');
+    table.boolean('verified').notNullable().defaultTo(false);
     table.timestamp('created_at').defaultTo(knex.fn.now());
     table.timestamp('updated_at').defaultTo(knex.fn.now());
+    table.string('owner_email').notNullable();
+    table
+      .foreign('owner_email')
+      .references('email')
+      .inTable('user_accounts')
+      .onDelete('CASCADE');
   });
 };
 
